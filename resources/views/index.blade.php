0bc1f22e5803211759a44fa8a783df07f5cd5513
zkaria <zakariya.laarrague@outlook.fr>
2023-11-03 23:45:56 +0100

    make code organaized

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.main')
+
+@section('content')
+    <@include('sections.about')>
+    <@include('sections.service')>
+    <@include('sections.portfolio')>
+    <@include('sections.pricing')>
+    <@include('sections.hire_me')>
+    <@include('sections.testmonial')>
+    <@include('sections.blog')>
+    <@include('sections.contact')>
+@endsection
+
+

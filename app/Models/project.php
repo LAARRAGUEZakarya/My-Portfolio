6a07dc4e1de12b7bcfbb0e787f4a9cde825bec96
zkaria <zakariya.laarrague@outlook.fr>
2023-11-04 17:44:15 +0100

    create all models with it's megrations i need now

---
+++ app/Models/project.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'url',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(category::class);
+    }
+}

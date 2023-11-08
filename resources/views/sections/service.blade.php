ad747ae80988cbf76e093320e9796a684237f59e
zkaria <zakariya.laarrague@outlook.fr>
2023-11-08 22:56:45 +0100

    services has been added successfuly

--- resources/views/sections/service.blade.php
+++ resources/views/sections/service.blade.php
@@ -5,39 +5,31 @@
         <h6 class="section-title mb-6">Service</h6>
         <!-- row -->
         <div class="row">
-            <div class="col-md-6 col-lg-3">
-                <div class="service-card">
-                    <div class="body">
-                        <img src="assets/imgs/pencil-case.svg" alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page" class="icon">
-                        <h6 class="title">Adipisicing</h6>
-                        <p class="subtitle">Labore velit culpa adipisci excepturi consequuntur itaque in nam molestias dolorem iste quod.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-3">
+
+            <div class="col-md-6 col-lg-4">
                 <div class="service-card">
                     <div class="body">
                         <img src="assets/imgs/responsive.svg" alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page" class="icon">
-                        <h6 class="title">Sapiente</h6>
-                        <p class="subtitle">Labore velit culpa adipisci excepturi consequuntur itaque in nam molestias dolorem iste quod.</p>
+                        <h6 class="title">Full-Stack Developer</h6>
+                        <p class="subtitle">Full-Stack Developer excelling in web, mobile, and desktop app development, delivering versatile digital solutions.</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-lg-3">
+            <div class="col-md-6 col-lg-4">
                 <div class="service-card">
                     <div class="body">
                         <img src="assets/imgs/toolbox.svg" alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page" class="icon">
-                        <h6 class="title">Placeat</h6>
-                        <p class="subtitle">Labore velit culpa adipisci excepturi consequuntur itaque in nam molestias dolorem iste quod.</p>
+                        <h6 class="title">IT Consultant</h6>
+                        <p class="subtitle">IT Consultant, your strategic tech partner for business excellence.</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-lg-3">
+            <div class="col-md-6 col-lg-4">
                 <div class="service-card">
                     <div class="body">
                         <img src="assets/imgs/analytics.svg" alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page" class="icon">
-                        <h6 class="title">Iusto</h6>
-                        <p class="subtitle">Labore velit culpa adipisci excepturi consequuntur itaque in nam molestias dolorem iste quod.</p>
+                        <h6 class="title">Digital Marketing</h6>
+                        <p class="subtitle">Digital Marketing , shaping your brand's online success with social media and ads.</p>
                     </div>
                 </div>
             </div>

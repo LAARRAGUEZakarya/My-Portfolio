475591e633ffa4db7172ed24232d470515a25501
zkaria <zakariya.laarrague@outlook.fr>
2023-11-10 17:28:04 +0100

    add linkdin link to footer icon linkdin

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,14 +1,15 @@
 <!-- footer -->
 <div class="container">
     <footer class="footer">
-        <p class="mb-0">Copyright 2023 &copy; <a href="https://www.linkedin.com/in/laarrague-zakarya-3085ba1bb/">LAARRAGUE Zakarya</a></p>
+        <p class="mb-0">Copyright 2023 &copy; <a href="https://www.linkedin.com/in/laarrague-zakarya-3085ba1bb/" target="_blank">LAARRAGUE Zakarya</a></p>
         <div class="social-links text-right m-auto ml-sm-auto">
-            <a href="javascript:void(0)" class="link"><i class="ti-facebook"></i></a>
-            <a href="javascript:void(0)" class="link"><i class="ti-twitter-alt"></i></a>
-            <a href="mailto:laarraguezakarya@gmail.com" class="link"><i class="ti-google"></i></a>
-            <a href="javascript:void(0)" class="link"><i class="ti-pinterest-alt"></i></a>
-            <a href="javascript:void(0)" class="link"><i class="ti-instagram"></i></a>
-            <a href="javascript:void(0)" class="link"><i class="ti-rss"></i></a>
+            <a href="javascript:void(0)" class="link" target="_blank"><i class="ti-facebook"></i></a>
+            <a href="javascript:void(0)" class="link" target="_blank"><i class="ti-twitter-alt"></i></a>
+            <a href="mailto:laarraguezakarya@gmail.com" class="link" target="_blank"><i class="ti-google"></i></a>
+            <a href="javascript:void(0)" class="link" target="_blank"><i class="ti-pinterest-alt" ></i></a>
+            <a href="javascript:void(0)" class="link" target="_blank"><i class="ti-instagram"></i></a>
+            <a href="https://www.linkedin.com/in/laarrague-zakarya-3085ba1bb/" class="link" target="_blank"><i class="ti-linkedin"></i></a>
+            <a href="javascript:void(0)" class="link" target="_blank"><i class="ti-rss"></i></a>
         </div>
     </footer>
 </div> <!-- end of page footer -->
